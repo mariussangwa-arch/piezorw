140e87247903b0025de337c297dbc19b2d0de871
Marius <mariussangwa@gmail.com>
2026-08-10 02:26:39 +0200

    added the djangoApp for new feature

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -52,6 +53,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -77,6 +79,23 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+
+LANGUAGES = [
+    ('en', 'English'),
+    ('rw', 'Kinyarwanda'),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
+
+from django.conf.locale import LANG_INFO
+LANG_INFO['rw'] = {
+    'bidi': False,
+    'name': 'Kinyarwanda',
+    'code': 'rw',
+    'name_local': 'Kinyarwanda',
+}
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
